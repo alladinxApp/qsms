1d3a2423deedd293366ba341a4de6a9c84e00ef4
Noel S. Rodriguez <noelsr70@yahoo.com>
2015-11-04 23:29:37 +0800

    Put Icon for Processed Work Order

--- workorder_menu.php
+++ workorder_menu.php
@@ -1,4 +1,4 @@
 <div class="menu_img">
-    <div style="text-align: center;"><a href="processed_workorder_list.php">Processed Work Order</a></div>
+    <div style="text-align: center;"><a href="processed_workorder_list.php"><img src="images/processed_wo.png" alt=""></a></div>
 </div>
 <br /><br /><hr noshade style="clear: both;"/>
