8a4fd28fffc11e11a3d886cf33be40d65f65e42a
Noel S. Rodriguez <noelsr70@yahoo.com>
2015-11-04 23:39:12 +0800

    Put Icon for Processed Estimate

--- estimate_menu.php
+++ estimate_menu.php
@@ -2,6 +2,6 @@
 	<div style="text-align: center;"><a href="estimate_add.php"><img src="images/create.png" alt="create work order"></a></div>
 	<div style="text-align: center;"><a href="estimate_for_approval.php"><img src="images/approval.png" alt="approval"></a></div>
     <div style="text-align: center;"><a href="online_estimate_list.php"><img src="images/online.png" alt="online"></a></div>
-    <div style="text-align: center;"><a href="processed_estimate_list.php">Processed Estimate</a></div>
+    <div style="text-align: center;"><a href="processed_estimate_list.php"><img src="images/processed_est.png" alt="online"></a></div>
 </div>
 <br /><br /><hr noshade style="clear: both;"/>
