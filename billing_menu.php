db97202aa43bd699026c90ccaefaa898292a3710
Noel S. Rodriguez <noelsr70@yahoo.com>
2015-11-04 23:35:47 +0800

    Put Icon for Billing Posting and Billing Posted

--- billing_menu.php
+++ billing_menu.php
@@ -3,7 +3,7 @@
 	<div style="text-align: center;"><a href="cash_billing_list.php"><img src="images/cashpay.png" alt=""></a></div>
     <div style="text-align: center;"><a href="card_billing_list.php"><img src="images/card.png" alt=""></a></div>
 	<div style="text-align: center;"><a href="billing_list.php"><img src="images/billing.png" alt=""></a></div>
-	<div style="text-align: center;"><a href="billing_for_posting.php">Billing for Posting</a></div>
-	<div style="text-align: center;"><a href="billing_posted.php">Billing Posted</a></div>
+	<div style="text-align: center;"><a href="billing_for_posting.php"><img src="images/posting.png" alt=""></a></div>
+	<div style="text-align: center;"><a href="billing_posted.php"><img src="images/processed_bills.png" alt=""></a></div>
 </div>
 <br /><br /><hr noshade style="clear: both;"/>
