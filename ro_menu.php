b76cf37f2ac765c95de15cf7432551cc828f2b4f
noelsr70 <noelsr70@yahoo.com>
2016-04-26 22:59:29 +0800

    Change Menu to Icon

--- ro_menu.php
+++ ro_menu.php
@@ -1,7 +1,7 @@
 <div class="menu_img">
-	<div style="text-align: center;"><a href="ro_add.php">Create RO</a></div>
-	<div style="text-align: center;"><a href="receiving.php">Receiving</a></div>
-    <div style="text-align: center;"><a href="rr_posting.php">RR Posting</a></div>
-    <div style="text-align: center;"><a href="payment.php">Payment</a></div>
+	<div style="text-align: center;"><a href="ro_add.php"><img src="images/purchaseprep.png" alt=""></div>
+	<div style="text-align: center;"><a href="receiving.php"><img src="images/recrep.png" alt=""></div>
+    <div style="text-align: center;"><a href="rr_posting.php"><img src="images/roposting.png" alt=""></div>
+    <div style="text-align: center;"><a href="payment.php"><img src="images/ropayment.png" alt=""></div>
 </div>
 <br /><br /><hr noshade style="clear: both;"/>
