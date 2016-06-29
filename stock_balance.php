187fbddc5d87d48050a3c1e11f5bc4e41562a6c1
Rey P. Castanares <alladinx@gmail.com>
2016-06-29 09:57:25 +0800

    commit stock balance report and export

---
+++ stock_balance.php
@@ -0,0 +1,66 @@
+<?php
+	session_start();
+	$ses_id = session_id();
+	require_once("conf/db_connection.php");
+	require_once("functions.php");
+
+	$sql_items = "SELECT * FROM tbl_items WHERE status = '1'";
+	$qry_items = mysql_query($sql_items);
+?>
+<html>
+<head>
+<title></title>
+<link rel="stylesheet" type="text/css" href="style/cal.css" />
+<link rel="stylesheet" type="text/css" href="style/forms.css" />  
+<? require_once('inc/datepicker.php'); ?>
+</head>
+
+<body>
+	<p id="title">Inventory Report</p>
+	<form method="Post" onSubmit="return valMe();" action="stock_balance_report_result.php" target="_blank">
+	<div class="estimate_approval">
+	<table>
+		<tr>
+			<td width="150">Date</td>
+			<td width="10" align="center">:</td>
+			<td width="125"><input type="text" id="txtdate" name="txtdate" readonly class="date-pick" style="width: 100"></td>
+		</tr>
+		<tr>
+			<td width="150">Item / SAP Code</td>
+			<td width="10" align="center">:</td>
+			<td align="center" width="125"><select name="txtitem" id="txtitem">
+				<option value="">All Items</option>
+				<? while($row = mysql_fetch_array($qry_items)) {?>
+				<option value="<?=$row['SAP_item_code'];?>"><?=$row['SAP_item_code'] . " - " . $row['item_description'];?></option>
+				<? } ?>
+			</select></td>
+		</tr>
+		<tr>
+			<td >&nbsp;</td>
+			<td align="center">&nbsp;</td>
+			<td colspan="3"><input type="submit" name="save" value="" style="cursor: pointer;"></td>
+		</tr>
+	</table>
+	</div>
+	<input type="hidden" id="export" name="export" value="1" />
+	<input type="hidden" id="report" name="report" value="invoicereport" />
+	</form>
+	<hr noshade style="clear: both;"/><br />
+	<script type="text/javascript">
+		function valMe(){
+			var dtfrom = document.getElementById("txtdatefrom");
+			var dtto = document.getElementById("txtdateto");
+			
+			if(dtfrom.value == ""){
+				alert("Please enter date from!");
+				return false;
+			}else if(dtto.value == ""){
+				alert("Please enter date to!");
+				return false;
+			}else{
+				return true;
+			}
+		}
+	</script>
+</body>
+</html>
---
+++ stock_balance_report_result.php
@@ -0,0 +1,110 @@
+<?php
+	session_start();
+	$ses_id = session_id();
+	require_once("conf/db_connection.php");
+	require_once("functions.php");
+
+	if(isset($_POST['export']) && !empty($_POST['export']) && $_POST['export'] == 1){
+		$date = dateFormat($_POST['txtdate'],"Y-m-d");
+		
+		$item = "ALL ITEMS";
+		if(!empty($_POST['txtitem'])){
+			$item = $_POST['txtitem'];
+		}
+
+		$dtfrom = $date . " 00:00:000";
+		$dtto = $date . " 23:59:000";
+		$dt = date("Ymdhis");
+
+		$itemdesc = "ALL ITEMS";
+		$itemcode = null;
+		if(!empty($_POST['txtitem'])){
+			$itemcode = "SAP_item_code = '$item' AND ";
+
+			$sql_item = "SELECT * FROM tbl_items WHERE SAP_item_code = '$item'";
+			$qry_item = mysql_query($sql_item);
+			while($row = mysql_fetch_array($qry_item)){
+				$itemdesc = $row['item_description'];
+			}
+		}
+
+		$sql_inv = "SELECT * FROM v_po_inventory
+				WHERE $itemcode v_po_inventory.created_date between '$dtfrom' AND '$dtto'
+				ORDER BY v_po_inventory.created_date";
+		$qry_inv = mysql_query($sql_inv);
+	}
+?>
+<html>
+<head>
+<title></title>
+<link rel="stylesheet" type="text/css" href="style/cal.css" />
+<link rel="stylesheet" type="text/css" href="style/forms.css" />  
+<? require_once('inc/datepicker.php'); ?>
+</head>
+<style>
+	div.divEstimateList{ height: 400px; width: 800px; border-left: 1px solid #ddd; border-top: 1px solid #ddd; }
+	div.divEstimateList table{ border: 1px solid #ccc; font-size: 12px; }
+	div.divEstimateList table th{ border-right: 1px solid #ccc; border-bottom: 1px solid #ccc; color: #fff; background: #0000ff; }
+</style>
+<body>
+	<p id="title">Inventory Report Result</p>
+	<? if(isset($_POST['export']) && !empty($_POST['export']) && $_POST['export'] == 1){ ?>
+	<form method="Post" action="export.php" target="_blank">
+	<input type="hidden" id="txtdate" name="txtdate" value="<?=$date;?>">
+	<input type="hidden" id="txtitem" name="txtitem" value="<?=!empty($_POST['txtitem']) ? $item : '';?>">
+	<table width="430">
+		<tr>
+			<td width="120">Date</td>
+			<td width="10">:</td>
+			<td width="300"><?=dateFormat($date,"F d, Y");?></td>
+		</tr>
+		<tr>
+			<td >SAP Code: </td>
+			<td>:</td>
+			<td ><?=$item;?></td>
+		</tr>
+		<tr>
+			<td >Item Description: </td>
+			<td>:</td>
+			<td ><?=$itemdesc;?></td>
+		</tr>
+	</table>
+	<div class="divEstimateList">
+	<table width="810">
+		<tr>
+			<th width="10">#</th>
+			<th width="100">Item Code</th>
+			<th width="100">SAP Code</th>
+			<th width="200">Description</th>
+			<th width="100">Ending Bal</th>
+		</tr>
+		<?
+			$cnt = 1; 
+			while($row = mysql_fetch_array($qry_inv)){
+
+				if($cnt%2){
+					$bg = "background: none;";
+				}else{
+					$bg = "background: #eee;";
+				}
+
+				$style = $bg;
+		?>
+		<tr>
+			<td><?=$cnt;?></td>
+			<td style="<?=$style;?>"><?=$row['item_code'];?></td>
+			<td style="<?=$style;?>"><?=$row['SAP_item_code'];?></td>
+			<td style="<?=$style;?>"><?=$row['item_description'];?></td>
+			<td style="<?=$style;?>" align="center"><?=$row['ending_balance'];?></td>
+		</tr>
+		<? $cnt++; } ?>
+		
+	</table>
+	</div>
+	<input type="submit" name="save" value="" style="cursor: pointer;">
+	<input type="hidden" id="export" name="export" value="1" />
+	<input type="hidden" id="report" name="report" value="stockbalancereport" />
+	</form>
+	<? } ?>
+</body>
+</html>
