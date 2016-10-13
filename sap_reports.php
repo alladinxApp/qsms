e7e15ecb0e2125409c0ef071b92cc0789356322d
noelsr70 <noelsr70@yahoo.com>
2016-10-13 11:27:36 +0800

    updates sap icons

--- sap_reports.php
+++ sap_reports.php
@@ -16,27 +16,27 @@
     <table width="210" border="0">
       <tr>
         <td width="15">&nbsp;</td>
-        <td width="89"><a href="oinv_report.php"><img src="images/OINV.png" width="78" height="100" alt="OINV"></a></td>
+        <td width="89"><a href="oinv_report.php"><img src="images/OINV.png" width="74" height="85" alt="OINV"></a></td>
         <td width="15">&nbsp;</td>
-        <td width="92"><a href="opdn_report.php"><img src="images/OPDN.png" width="78" height="100" alt="OPDN"></a></td>
+        <td width="92"><a href="opdn_report.php"><img src="images/OPDN.png" width="74" height="85" alt="OPDN"></a></td>
         <td width="15">&nbsp;</td>
-        <td width="92"><a href="pdn_report.php"><img src="images/PDN1.png" width="78" height="100" alt="PDN"></a></td>
+        <td width="92"><a href="pdn_report.php"><img src="images/PDN1.png" width="74" height="85" alt="PDN"></a></td>
         <td width="15">&nbsp;</td>
-		    <td width="92"><a href="oign_report.php"><img src="images/OIGN.png" width="78" height="100" alt="OIGN"></a></td>
+		    <td width="92"><a href="oign_report.php"><img src="images/OIGN.png" width="74" height="85" alt="OIGN"></a></td>
         <td width="15">&nbsp;</td>
-		    <td width="92"><a href="ign_report.php"><img src="images/IGN1.png" width="78" height="100" alt="IGN"></a></td>
+		    <td width="92"><a href="ign_report.php"><img src="images/IGN1.png" width="74" height="85" alt="IGN"></a></td>
         <td width="15">&nbsp;</td>
-		    <td width="92"><a href="orct_report.php"><img src="images/ORCT.png" width="78" height="100" alt="ORCT"></a></td>
+		    <td width="92"><a href="orct_report.php"><img src="images/ORCT.png" width="74" height="85" alt="ORCT"></a></td>
         <td width="15">&nbsp;</td>
-        <td width="92"><a href="checks_report.php"><img src="images/checks.png" width="78" height="100" alt="CHECKS"></a></td>
+        <td width="92"><a href="checks_report.php"><img src="images/checks.png" width="74" height="85" alt="CHECKS"></a></td>
         <td width="15">&nbsp;</td>
-        <td width="92"><a href="credit_card_report.php"><img src="images/creditcard.png" width="78" height="100" alt="CREDIT CARD"></a></td>
+        <td width="92"><a href="credit_card_report.php"><img src="images/creditcard.png" width="74" height="85" alt="CREDIT CARD"></a></td>
         <td width="15">&nbsp;</td>
       </tr>
 
       <tr>
         <td width="15">&nbsp;</td>
-        <td width="92"><a href="pmt_invoice_report.php"><img src="images/PMT_invoice.png" width="78" height="100" alt="PMT INVOICE"></a></td>
+        <td width="92"><a href="pmt_invoice_report.php"><img src="images/PMT_invoice.png" width="74" height="85" alt="PMT INVOICE"></a></td>
         <td width="15">&nbsp;</td>
         <td width="92">&nbsp;</td>
         <td width="15">&nbsp;</td>
